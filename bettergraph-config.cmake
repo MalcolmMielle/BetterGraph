61714da1a4c04640ef32d418dfe2999c14f31517
Malcolm Mielle <malcolm.mielle@protonmail.com>
2018-02-21 15:55:35 +0100

    correct config file

--- bettergraph-config.cmake
+++ bettergraph-config.cmake
@@ -1,3 +1,3 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 # include(${SELF_DIR}/editdistance-targets.cmake)
-get_filename_component(bettergraph_INCLUDE_DIRS "${SELF_DIR}/../../include/editdistance" ABSOLUTE)
+get_filename_component(bettergraph_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
